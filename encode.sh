de0128cdf2a213a88f7d43df5f804f759cb576fa
Sam Brenner <brenners@si.edu>
2014-09-24 16:12:58 -0400

    more sizes

--- encode.sh
+++ encode.sh
@@ -15,14 +15,18 @@ do
 	echo "begin encoding $base_file"
 	
 	# echo "making ogg..."
-	# ffmpeg -i $f -acodec libvorbis -ac 2 -ab 96k -ar 44100 -vb 6000k -vf scale=1920:-1 "${ENCODED_DIR}${base_filename}.ogv"
+	# ffmpeg -i $f -acodec libvorbis -ac 2 -ab 96k -ar 44100 -vb 6000k -vf scale=-1:1080 "${ENCODED_DIR}${base_filename}.ogv"
 
 	# echo "making webm..."
-	# ffmpeg -i $f -acodec libvorbis -ac 2 -ab 96k -ar 44100 -vb 6000k -vf scale=1920:-1 "${ENCODED_DIR}${base_filename}.webm"
+	# ffmpeg -i $f -acodec libvorbis -ac 2 -ab 96k -ar 44100 -vb 6000k -vf scale=-1:1080 "${ENCODED_DIR}${base_filename}.webm"
 	
-	echo "making mp4..."
-	ffmpeg -y -i $f -codec:v libx264 -preset slow -b:v 6000k -vf scale=1920:-2 -threads 0 -pass 1 -an -f mp4 /dev/null
-	ffmpeg -y -i $f -codec:v libx264 -preset slow -b:v 6000k -vf scale=1920:-2 -threads 0 -pass 2 -codec:a libfaac -b:a 128k -f mp4 "${ENCODED_DIR}${base_filename}.mp4"
+	echo "making 1080 mp4..."
+	ffmpeg -y -i $f -codec:v libx264 -preset slow -b:v 6000k -vf scale=-1:1080 -threads 0 -pass 1 -an -f mp4 /dev/null
+	ffmpeg -y -i $f -codec:v libx264 -preset slow -b:v 6000k -vf scale=-1:1080 -threads 0 -pass 2 -codec:a libfaac -b:a 128k -f mp4 "${ENCODED_DIR}${base_filename}_1080.mp4"
+
+	echo "making 720 mp4..."
+	ffmpeg -y -i $f -codec:v libx264 -preset slow -b:v 6000k -vf scale=-1:1080 -threads 0 -pass 1 -an -f mp4 /dev/null
+	ffmpeg -y -i $f -codec:v libx264 -preset slow -b:v 6000k -vf scale=-1:1080 -threads 0 -pass 2 -codec:a libfaac -b:a 128k -f mp4 "${ENCODED_DIR}${base_filename}_720.mp4"
 
 	echo "finished encoding $base_file"
 	mv $f "${POST_ENCODE_DIR}${base_file}"
